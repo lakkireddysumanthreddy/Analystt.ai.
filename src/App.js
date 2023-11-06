1b4e115095c3900ec5d3a5a7a6d2be7fd18a4f5f
unknown <lakkireddysumanthreddy.95025@gmail.com>
2023-11-06 23:20:55 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css"
 function App() {
+  
+  const [count, setCount] = useState(0);
+
+  
+  const handleButtonClick = () => {
+    setCount(count + 1);
+  };
+
+  
+  useEffect(() => {
+    console.log("Component is mounted");
+  }, []);
+
+  
+  useEffect(() => {
+    console.log("State changed: Count is now " + count);
+  }, [count]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="count">{count}</h1>
+      <button className="addButton" onClick={handleButtonClick}>Add</button>
     </div>
   );
 }
